b8944ca5457d48dde5941e9bd9029fed3f657bc0
rhodricgana <rhodricgana@gmail.com>
2022-03-04 12:26:33 +0000

    second maven

---
+++ src/main/java/gana/App.java
@@ -0,0 +1,13 @@
+package gana;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I love you my children, wife, my parens, my brothers and sisters. God bless you all" );
+    }
+}
